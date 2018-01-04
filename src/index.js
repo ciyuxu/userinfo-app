a613b8aadca62c2e83033dad710b130b0604d548
ciyuxu <ciyuxu@gmail.com>
2018-01-04 11:14:16 +0100

    Set-up Reducer and index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ConnectedRouter as Router } from "react-router-redux";
+import { Provider } from "react-redux";
+import store, { history } from "./store";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    
+      <App />
+    
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
